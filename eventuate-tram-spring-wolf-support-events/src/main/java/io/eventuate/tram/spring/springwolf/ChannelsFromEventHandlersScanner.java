4f8e694d9d8bf7a7dbfdf763cdd9c1c69bf879d6
Chris Richardson <chris@chrisrichardson.net>
2025-02-23 21:09:15 +0900

    WIP Make scanners more functional by moving class registration to Manager classes.

--- eventuate-tram-spring-wolf-support-events/src/main/java/io/eventuate/tram/spring/springwolf/ChannelsFromEventHandlersScanner.java
+++ eventuate-tram-spring-wolf-support-events/src/main/java/io/eventuate/tram/spring/springwolf/ChannelsFromEventHandlersScanner.java
@@ -2,21 +2,16 @@ package io.eventuate.tram.spring.springwolf;
 
 import io.eventuate.tram.events.subscriber.DomainEventDispatcher;
 import io.eventuate.tram.events.subscriber.DomainEventHandler;
-import io.eventuate.tram.events.subscriber.DomainEventHandlers;
 import io.github.springwolf.asyncapi.v3.model.channel.ChannelObject;
-import io.github.springwolf.asyncapi.v3.model.channel.message.MessageObject;
-import io.github.springwolf.asyncapi.v3.model.channel.message.MessageReference;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-@org.springframework.stereotype.Component
-public class ChannelsFromEventHandlersScanner implements EventuateTramChannelsScanner{
-
-  @Autowired
-  private SpringWolfMessageFactory springWolfMessageFactory;
+@Component
+public class ChannelsFromEventHandlersScanner implements EventuateTramChannelsScanner {
 
   private final List<DomainEventDispatcher> domainEventDispatchers;
 
@@ -26,34 +21,34 @@ public class ChannelsFromEventHandlersScanner implements EventuateTramChannelsSc
 
   public ElementsWithClasses<ChannelObject> scan() {
 
-    List<DomainEventHandlers> domainEventHandlers = domainEventDispatchers.stream()
+    Map<String, List<DomainEventHandler>> aggregateTypeToEvents = domainEventDispatchers.stream()
         .map(DomainEventDispatcher::getDomainEventHandlers)
-        .toList();
-
-    Map<String, List<DomainEventHandler>> aggregateTypeToEvents = domainEventHandlers.stream()
         .flatMap(dehs -> dehs.getHandlers().stream())
         .collect(Collectors.groupingBy(DomainEventHandler::getAggregateType));
 
-    return new ElementsWithClasses<>(aggregateTypeToEvents.entrySet().stream()
+    Map<String, ElementWithClasses<ChannelObject>> channelsWithClasses = aggregateTypeToEvents.entrySet().stream()
         .collect(Collectors.toMap(
             Map.Entry::getKey,
-            entry -> makeChannelObject(entry.getKey(), entry.getValue())
-        )));
+            entry -> makeChannelObjectWithClasses(entry.getKey(), entry.getValue())
+        ));
+
+    return ElementsWithClasses.make(channelsWithClasses);
   }
 
-  private ChannelObject makeChannelObject(String aggregateType, List<DomainEventHandler> eventHandlers) {
-    return ChannelObject.builder()
+  private ElementWithClasses<ChannelObject> makeChannelObjectWithClasses(String aggregateType, List<DomainEventHandler> eventHandlers) {
+    Set<Class<?>> classes = eventHandlers.stream()
+        .map(DomainEventHandler::getEventClass)
+        .collect(Collectors.toSet());
+
+    ChannelObject channelObject = ChannelObject.builder()
         .channelId(aggregateType)
-        .messages(eventHandlers.stream()
+        .messages(classes.stream()
             .collect(Collectors.toMap(
-                deh -> deh.getEventClass().getName(), // key mapper
-                this::makeMessageReference // value mapper
+                Class::getName,
+                SpringWolfUtils::makeMessageReference
             )))
         .build();
-  }
 
-  private MessageReference makeMessageReference(DomainEventHandler deh) {
-    MessageObject message = springWolfMessageFactory.makeMessageFromClass(deh.getEventClass());
-    return MessageReference.toComponentMessage(message);
+    return new ElementWithClasses<>(channelObject, classes);
   }
 }
