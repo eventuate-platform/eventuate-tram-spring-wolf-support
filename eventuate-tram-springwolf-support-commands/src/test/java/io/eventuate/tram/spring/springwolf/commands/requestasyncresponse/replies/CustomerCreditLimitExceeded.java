b2972e8c0bd4ce6e37c35b85d74eb304786989cf
Chris Richardson <Chris@ChrisRichardson.net>
2025-10-31 13:48:07 +0900

    Removed annotations that are actually part of Eventuate Tram

--- eventuate-tram-springwolf-support-commands/src/test/java/io/eventuate/tram/spring/springwolf/commands/requestasyncresponse/replies/CustomerCreditLimitExceeded.java
+++ eventuate-tram-springwolf-support-commands/src/test/java/io/eventuate/tram/spring/springwolf/commands/requestasyncresponse/replies/CustomerCreditLimitExceeded.java
@@ -1,5 +1,7 @@
 package io.eventuate.tram.spring.springwolf.commands.requestasyncresponse.replies;
 
+import io.eventuate.tram.commands.consumer.annotations.FailureReply;
+
 @FailureReply
 public class CustomerCreditLimitExceeded implements ReserveCreditResult {
 }
--- eventuate-tram-springwolf-support-commands/src/test/java/io/eventuate/tram/spring/springwolf/commands/requestasyncresponse/replies/CustomerCreditReserved.java
+++ eventuate-tram-springwolf-support-commands/src/test/java/io/eventuate/tram/spring/springwolf/commands/requestasyncresponse/replies/CustomerCreditReserved.java
@@ -1,5 +1,7 @@
 package io.eventuate.tram.spring.springwolf.commands.requestasyncresponse.replies;
 
+import io.eventuate.tram.commands.consumer.annotations.SuccessReply;
+
 @SuccessReply
 public class CustomerCreditReserved implements ReserveCreditResult {
 }
